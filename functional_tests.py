8ffbf47e5d69959f2f81cb70ca1a3b2045148ab5
VelogaWhite <gunyok2515@gmail.com>
2026-01-14 19:10:55 +0700

    FT EDIT

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # This is your new helper method
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -23,7 +29,22 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
-           # There is still a text box inviting her to add another item.
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys("Buy peacock feathers")
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        # HERE IS THE CHANGE: We use the helper method
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
         inputbox = self.browser.find_element(By.ID, "id_new_item")
@@ -32,16 +53,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows],
-        )
-        self.assertIn(
-            "1: Buy peacock feathers",
-            [row.text for row in rows],
-        )
+        # HERE IS THE CHANGE: We use the helper method twice
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # Satisfied, she goes back to sleep
 
