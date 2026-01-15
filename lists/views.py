0f83e0db01a9601bbfe8282cb368db3dc15f7074
VelogaWhite <gunyok2515@gmail.com>
2026-01-15 15:23:15 +0700

    Add Priority's Text Box in home.html

--- lists/views.py
+++ lists/views.py
@@ -12,10 +12,10 @@ def new_list(request):
     nulist = List.objects.create()
     nupriority = Priority.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist, priority=nupriority)
-    return redirect(f"/lists/{nulist.id}/")
+    return redirect(f"/lists/{nulist.id}/{nupriority.id}/")
 
 def add_item(request, list_id, priority_id):
     our_list = List.objects.get(id=list_id)
     our_priority = Priority.objects.get(id=priority_id)
-    Item.objects.create(text=request.POST["item_text"], list=our_priority,)
-    return redirect(f"/lists/{our_list.id}/")
+    Item.objects.create(text=request.POST["item_text"], list=our_priority, priority=our_priority)
+    return redirect(f"/lists/{our_list.id}/{our_priority.id}/")
