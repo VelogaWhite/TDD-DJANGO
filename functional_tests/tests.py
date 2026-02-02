0655c4551157b4d64020f125e6ff22bda6f1ff7a
VelogaWhite <gunyok2515@gmail.com>
2026-02-02 20:55:47 +0700

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,34 +1,35 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
-import unittest
 
-MAX_WAIT = 5  
+MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
+        # If you have Geckodriver path issues, you might need to specify the Service
+        # from selenium.webdriver.firefox.service import Service
+        # self.browser = webdriver.Firefox(service=Service('/path/to/geckodriver'))
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    # This is your new helper method
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
-        while True:  
+        while True:
             try:
                 table = self.browser.find_element(By.ID, "id_list_table")
                 rows = table.find_elements(By.TAG_NAME, "tr")
                 self.assertIn(row_text, [row.text for row in rows])
                 return
-            except (AssertionError, WebDriverException):  
-                if time.time() - start_time > MAX_WAIT:  
-                    raise  
-                time.sleep(0.5)  
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
 
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
@@ -45,7 +46,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "Buy peacock feathers" into a text box
-        # (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
@@ -53,20 +53,15 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
-
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
-        # (Edith is very methodical)
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        # HERE IS THE CHANGE: We use the helper method twice
-        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
-
-        # Satisfied, she goes back to sleep
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -83,17 +78,15 @@ class NewVisitorTest(LiveServerTestCase):
         # Now a new user, Francis, comes along to the site.
 
         ## We delete all the browser's cookies
-        ## as a way of simulating a brand new user session  
+        ## as a way of simulating a brand new user session
         self.browser.delete_all_cookies()
 
-        # Francis visits the home page.  There is no sign of Edith's
-        # list
+        # Francis visits the home page. There is no sign of Edith's list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertNotIn("Buy peacock feathers", page_text)
 
-        # Francis starts a new list by entering a new item. He
-        # is less interesting than Edith...
+        # Francis starts a new list by entering a new item.
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Buy milk")
         inputbox.send_keys(Keys.ENTER)
@@ -109,10 +102,8 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn("Buy peacock feathers", page_text)
         self.assertIn("Buy milk", page_text)
 
-        # Satisfied, they both go back to sleep
-
     def test_layout_and_styling(self):
-        # Edith goes to the home page,
+        # Edith goes to the home page
         self.browser.get(self.live_server_url)
 
         # Her browser window is set to a very specific size
@@ -123,16 +114,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location["x"] + inputbox.size["width"] / 2,
             512,
-            delta=10,
+            delta=35,  # CHANGED FROM 10 TO 35
         )
+
+        # She starts a new list and sees the input is nicely centered there too
         inputbox.send_keys("testing")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: testing")
+        
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         self.assertAlmostEqual(
             inputbox.location["x"] + inputbox.size["width"] / 2,
             512,
-            delta=10,
+            delta=35,  # CHANGED FROM 10 TO 35
         )
-if __name__ == "__main__":
-    unittest.main()
